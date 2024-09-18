2a4da70c33b45eb255adfeae24a3e0485d1edb6b
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-18 14:05:26 -0300

    Add files via upload

---
+++ atividade-tema11-2.py
@@ -0,0 +1,6 @@
+# Solicitar um valor do usuário
+valor = int(input("Digite um valor: "))
+
+# Verificar se o valor é menor ou igual a 35
+if valor <= 35:
+    print("O valor digitado é menor ou igual a 35!")
